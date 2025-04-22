020fc3d364dd7c7e676c86b7f20036b010b8008e
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-04-22 22:31:08 +0200

    Create 1.3_Greater than or equal to.py

---
+++ Part 2/1.3_Greater than or equal to.py
@@ -0,0 +1,12 @@
+# Write your solution here
+first = int(input("Please type in the first number: "))
+second = int(input("Please type in another number: "))
+
+if first > second:
+    print(f"The greater number was: {first}")
+
+elif first < second:
+    print(f"The greater number was: {second}")
+
+elif first == second:
+    print("The numbers are equal!")
