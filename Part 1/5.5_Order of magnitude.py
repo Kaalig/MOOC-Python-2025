cc96209b7edc4b7ef185ba24b4bcb7e0fbe53c0f
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-04-22 22:24:59 +0200

    Create 5.5_Order of magnitude.py
    A REFAIRE. TOUJOURS PAS FINI.

---
+++ Part 1/5.5_Order of magnitude.py
@@ -0,0 +1,14 @@
+# Write your solution here
+
+number = int(input("Please type in a number: "))
+
+if number > 1000:
+    print("Thank you!")
+else:
+    print("This number is smaller than 1000")
+    print("Thank you!")
+
+if number < 100:
+    print("This number is smaller than 1000")
+    print("This number is smaller than 100")
+    print("Thank you!")
