fa9867bbb983571a5040e27e7ca35a7fd5baea8a
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-04-22 22:20:50 +0200

    Create 4.5_Sum and product.py

---
+++ Part 1/4.5_Sum and product.py
@@ -0,0 +1,8 @@
+# Write your solution here
+nbr1 = int(input("Number 1: "))
+nbr2 = int(input("Number 2: "))
+sum = nbr1 + nbr2
+product = nbr1 * nbr2
+
+print(f"The sum of the numbers: {sum}")
+print(f"The product of the numbers: {product}")
