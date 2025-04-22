084b9d844f9489ef7757a0d3e46196a8d2982600
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-04-22 22:15:53 +0200

    Create 2.3_Name and address.py

---
+++ Part 1/2.3_Name and address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+user_name = input("Given name: ")
+fam_name = input("Family name: ")
+St_address = input("Street address: ")
+cp_code = input("City and postal code: ")
+
+print(user_name + " " + fam_name)
+print(St_address)
+print(cp_code)
