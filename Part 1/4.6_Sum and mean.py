0a5002a3b399e4a40b1b6af5aa6c03291a377ace
Kaalig <116390704+Kaalig@users.noreply.github.com>
2025-04-22 22:21:19 +0200

    Create 4.6_Sum and mean.py

---
+++ Part 1/4.6_Sum and mean.py
@@ -0,0 +1,10 @@
+# Write your solution here
+
+var1 = int(input("Number 1: "))
+var2 = int(input("Number 2: "))
+var3 = int(input("Number 3: "))
+var4 = int(input("Number 4: "))
+sum = var1 + var2 + var3 + var4
+mean = (var1 + var2 + var3 + var4) / 4
+
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
